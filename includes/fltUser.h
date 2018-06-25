fd43c3b2e2a86960d961cf842acb7a8c8abfdb35
omri <omric500@gmail.com>
2018-06-25 23:44:24 +0300

    deleted all old files

--- includes/fltUser.h
+++
@@ -1,3 +0,0 @@
-//TODO: write fltUser.h
-//This is a header file used in order to give the user-mode application access to kernel mode structres
-//The header file will implement structs from the msdn in order to allow the user-mode application an option to read information about the file
